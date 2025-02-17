c23bec11ffc9e62c08b3e381d67114194865104a
Biel-mendes <gabrielmcportas@gmail.com>
2025-02-17 10:34:01 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+import { View , Text} from 'react-native';
+import Exemplo3 from "./exemplo3"
+
+const Fatec = () => {
+  return(
+    <View>
+      <Text>FATEC Zona leste</Text>
+      <Exemplo3 />
+    </View>
+    );
+};
+
+export default Fatec;
---
+++ exemplo3.tsx
@@ -0,0 +1,15 @@
+import react from 'React'
+
+// import Fatec from "./app.tsx"
+
+import { View , Text} from 'react-native'
+
+const Exemplo3 = () =>{
+    return(
+        <View>
+            <Text>Bem vindo ao curso de dsm</Text>
+        </View>
+    );
+};
+
+export default Exemplo3;
