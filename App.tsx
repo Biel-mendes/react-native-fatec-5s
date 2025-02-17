3b930644232d1bac139f8930ae51efa8fd0c71d4
Biel-mendes <gabrielmcportas@gmail.com>
2025-02-17 11:09:55 -0300

    umas paradas ai

--- App.tsx
+++ App.tsx
@@ -1,16 +1,39 @@
 
 
-import React from 'react';
-import { View , Text} from 'react-native';
-import Exemplo3 from "./exemplo3"
+import { useState } from 'react';
+
+import { View , Text , StyleSheet} from 'react-native';
+
 
 const Fatec = () => {
+  const [titleText, TesteTexto] = useState("veja a menssagem [aberte aqui]");
+  const bodyText = 'melhor faculdade de tecnologia';
+
+  const onPressTitle = () => {
+    TesteTexto('Fatec Zona Leste');
+  }
+
   return(
     <View>
-      <Text>FATEC Zona leste</Text>
-      <Exemplo3 />
+      <Text style={styles.titleText} onPress={onPressTitle}>
+        {titleText}
+        {'\n'}
+        {'\n'}
+      </Text>
+      <Text numberOfLines={5}>{bodyText}</Text>
     </View>
-    );
+  );
+
 };
+
+const styles = StyleSheet.create({
+  baseText: {
+    fontFamily: 'Cochin',
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
 
 export default Fatec;
